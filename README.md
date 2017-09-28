361660f707cddc9d00c21c59f621cd9fb2de379f
Ryan Franco <rfranco92@gmail.com>
2017-09-28 14:42:35 -0400

    Finished Project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # liri-node-app
+
+How to Use Liri
+
+MAKE SURE YOU INSTALL THE NODE PACKAGES!!! YOU NEED THEM TO RUN THIS PROGRAM!!!
+
+my-tweets - gets recent twitter statuses
+
+spotify-this-song - pulls data from spotify from whatever song you input next to it. If there is none, it will default to Ace of Base.
+
+movie-this - pulls data from OMDB on whatever movie you input next to it. If there is none, it will default to Mr. Nobody.
+
+do-what-it-says - pulls whatever is inputed on random.txt and then runs the function. You can change the data but you MUST include a comma!
--- liri.js
+++ liri.js
@@ -13,93 +13,92 @@ var spotifyClient = new spotify({
 	id: "8acc70f1b35f476dbe4a19604a639a12",
   	secret: "f5a34c486099444c9833dbfb6341b8cf"
 });
-	var fs = require ("fs");
+var fs = require ("fs");
 
-	var nodeArgs = process.argv;
-	var liristring = ""
+//Converts Process argv into a variable nodeArgs, creates variable liri string which adds on whatever is added in process.argv[3] or more 
+var nodeArgs = process.argv;
+var liristring = ""
 
-	var liriCommand = nodeArgs[2];
-	for (var i = 3; i < nodeArgs.length; i++) {
-
+var liriCommand = nodeArgs[2];
+for (var i = 3; i < nodeArgs.length; i++) {
   if (i > 3 && i < nodeArgs.length) {
-
     liristring = liristring + "+" + nodeArgs[i];
-
   }
-
   else {
-
     liristring += nodeArgs[i];
-
   }
-	}
-	function runLIRI(){
+}
+
+//main function RUNLIRI, which is split into 4 if else statements.
+function runLIRI(){
 	if (liriCommand === "my-tweets"){
-	twitterclient.get('statuses/user_timeline', function(error, tweets, response) {
- 	 if(error) throw error;
- 	 for (var i = 0; i < tweets.length; i++){
- 	 console.log ("Tweet Number " + (i+1))
- 	 console.log(tweets[i].created_at); 
- 	 console.log(tweets[i].text);
- 	 console.log("-----------------")
- 	}
-	});
+		//twitter client gets info from statuses and timelines
+		twitterclient.get('statuses/user_timeline', function(error, tweets, response) {
+ 			 if(error) throw error;
+ 			 for (var i = 0; i < tweets.length; i++){
+ 	 			console.log ("Tweet Number " + (i+1))
+ 	 			console.log(tweets[i].created_at); 
+ 	 			console.log(tweets[i].text);
+ 	 			console.log("-----------------")
+ 				}
+			});
 	}
 	else if (liriCommand === "spotify-this-song"){
-	if(liristring === ""){
-		liristring = "Ace of Base";
-		console.log(liristring);
-	}
-
-
-
-	spotifyClient.search({ type: 'track', query: liristring, limit: 1 }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-  else{
-  	var songInfo = data.tracks.items[0];
-    	console.log("Name of the Artist: " + songInfo.artists[0].name);
-        console.log("Name of the Song: " + songInfo.name);
-        console.log("Link to the Song on Spotify: " + songInfo.external_urls.spotify);
-        console.log("Name of the Album: " + songInfo.album.name);
- 	 }
-	});
-	}
-	else if (liriCommand === "movie-this"){
-	if(liristring === ""){
-		liristring = "Mr. Nobody";
-		console.log("If You Haven't Watched Mr. Nobody, Then You Should:  http://www.imdb.com/title/tt0485947/");
-		console.log("It's on Netflix");
-	}
-	else{
-		var queryUrl = "http://www.omdbapi.com/?t=" + liristring + "&y=&plot=short&apikey=40e9cece";
-		request(queryUrl, function(error, response, body) {
-
-  		// If the request is successful
- 		 if (!error && response.statusCode === 200) {
-   		 console.log("Title: " + JSON.parse(body).Title);
-   		 console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-   		 console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-   		 console.log("Country: " + JSON.parse(body).Country);
-   		 console.log("Language: " + JSON.parse(body).Language);
-   		 console.log("Plot: " + JSON.parse(body).Plot);
-   		 console.log("Actors: " + JSON.parse(body).Actors);
-  		}
-		});
+		//gets info from Spotify, if liristring was unchanged it becomes Ace of Base
+		if(liristring === ""){
+			liristring = "Ace of Base";
+			console.log(liristring);
 		}
+		spotifyClient.search({ type: 'track', query: liristring, limit: 1 }, function(err, data) {
+  		if (err) {
+    		return console.log('Error occurred: ' + err);
+  		}
+  		else{
+  			var songInfo = data.tracks.items[0];
+  			for (var j = 0; j < songInfo.artists.length; j++){
+    			console.log("Name of the Artist: " + songInfo.artists[j].name);
+    	}
+        	console.log("Name of the Song: " + songInfo.name);
+        	console.log("Link to the Song on Spotify: " + songInfo.external_urls.spotify);
+        	console.log("Name of the Album: " + songInfo.album.name);
+ 	 	}
+	});
+ }
+
+	else if (liriCommand === "movie-this"){
+		//Gets Info From OMDB, if there is none it becomes Mr. Nobody
+		if(liristring === ""){
+			liristring = "Mr. Nobody";
+			console.log("If You Haven't Watched Mr. Nobody, Then You Should:  http://www.imdb.com/title/tt0485947/");
+			console.log("It's on Netflix");
+		}
+			var queryUrl = "http://www.omdbapi.com/?t=" + liristring + "&y=&plot=short&apikey=40e9cece";
+			request(queryUrl, function(error, response, body) {
+
+  			// If the request is successful
+ 		 		if (!error && response.statusCode === 200) {
+   		 		console.log("Title: " + JSON.parse(body).Title);
+   		 		console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+   		 		console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+   		 		console.log("Country: " + JSON.parse(body).Country);
+   		 		console.log("Language: " + JSON.parse(body).Language);
+   		 		console.log("Plot: " + JSON.parse(body).Plot);
+   		 		console.log("Actors: " + JSON.parse(body).Actors);
+  				}
+			});
 	}
 	else if (liriCommand === "do-what-it-says"){
+		//reads random.txt, splices the comma, and puts each side of the array into command and string, then runs the function.
 		fs.readFile("random.txt", "utf8", function(err, data) {
  		 if (err) {
-   		 return console.log(err);
-  		}
+   		 	return console.log(err);
+  		 }
 
-  	var output = data.split(",");
-  	liriCommand = output[0];
-  	liristring = output[1];
-	runLIRI();
-	});
+  		var output = data.split(",");
+  		liriCommand = output[0];
+  		liristring = output[1];
+		runLIRI();
+		});
 	}
 }
 
