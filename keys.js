497ae2d2e231ec5092415884e9be1d3fb65ce7e0
Ryan Franco <rfranco92@gmail.com>
2017-09-28 09:47:39 -0400

    Works properly

---
+++ keys.js
@@ -0,0 +1,8 @@
+var twitterKeys = {
+  consumer_key: 'mzJFqMphFOymY7QBQYaQNyhCV',
+  consumer_secret: 'g2tJB4fwsA4IVNyGtJNzHgXzja2MLivQwnIIuhcXNayjcZTpsk',
+  access_token_key: '912853730403418112-9dq7wdDNCenR29gcmELrFu2hqfignBK',
+  access_token_secret: 'buSbXwwBUhk9MoyBkKVwxpBxoOIuTMJJdEvF4yPj3CAbI',
+}
+
+module.exports = twitterKeys;
